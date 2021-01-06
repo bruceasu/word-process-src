af4b24279124fe375f82ca6f90e15d209ab88ab5
bruceasu <bruceasu@163.com>
2021-01-06 23:33:51 +0800

    update.

--- src/main/java/me/asu/phrase/CleanupPhrases2.java
+++ src/main/java/me/asu/phrase/CleanupPhrases2.java
@@ -3,6 +3,10 @@ package me.asu.phrase;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -11,7 +15,7 @@ import me.asu.word.ResourcesFiles;
 /**
  * @author suk
  */
-public class CleanupPhrases
+public class CleanupPhrases2
 {
 
 	public static void main(String[] args) throws IOException
@@ -40,32 +44,58 @@ public class CleanupPhrases
 				singleWords.add(kv[0]);
 			}
 		});
-
-		File       out1 = new File("out", "c.txt");
-		File       out2 = new File("out", "d.txt");
+		// 精简无重
+		File       out1 = new File("out", "a.txt");
+		// 精简有重
+		File       out2 = new File("out", "b.txt");
+		// 其他无重
+		File       out3 = new File("out", "c.txt");
+		// 其他有重
+		File       out4 = new File("out", "d.txt");
 		FileWriter fw1  = new FileWriter(out1);
 		FileWriter fw2  = new FileWriter(out2);
-
+		FileWriter fw3  = new FileWriter(out3);
+		FileWriter fw4  = new FileWriter(out4);
+		
 		List<String> nouns    = ResourcesFiles.readLinesInResources("名词.txt");
 		Set<String>  nounsSet = new HashSet<>();
 		nounsSet.addAll(nouns);
 
-		cleanupPhrases3(codeSet, phraseCodeSet, nounsSet, fw1, fw2);
+		Set<String> cp = new HashSet<>(ResourcesFiles.readLinesInResources("精简词汇.txt"));
+		
+		List<String> list = ResourcesFiles.readLinesInResources("系统词库57000.txt");
+		List<String> L1 = new ArrayList<>();
+		List<String> L2 = new ArrayList<>();
+		list.forEach(s->{
+			if (s == null || s.trim().isEmpty()) {
+				return;
+			}
+			String[] kv = s.trim().split("\\s+");
+			if (cp.contains(kv[0])) {
+				L1.add(s.trim());
+			} else {
+				L2.add(s.trim());
+			}
+		});
+		
+		cleanupPhrases1(L1, codeSet, phraseCodeSet, nounsSet, fw1, fw2);
 
-		cleanupPhrases4(codeSet, phraseCodeSet, nounsSet, freqWords, singleWords, fw1, fw2);
+		cleanupPhrases2(L2, codeSet, phraseCodeSet, nounsSet, freqWords, singleWords, fw3, fw4);
 
 		fw1.close();
 		fw2.close();
+		fw3.close();
+		fw4.close();
 		System.out.println("Done!");
 	}
 
-	private static void cleanupPhrases4(Set<String> codeSet, Set<String> phraseCodeSet, Set<String> nounsSet,
+	private static void cleanupPhrases2(List<String> phrases, Set<String> codeSet, Set<String> phraseCodeSet, Set<String> nounsSet,
 			Set<String> freqWords, Set<String> singleWords, FileWriter fw1, FileWriter fw2) {
-		List<String> phrases4 = ResourcesFiles.readLinesInResources("phrases-4.txt");
-		phrases4.forEach(line -> {
+		phrases.forEach(line -> {
 			if (line.trim().isEmpty()) {
 				return;
 			}
+			
 			String[] kv      = line.trim().split("\\s+");
 			boolean  isNoun  = nounsSet.contains(kv[0]);
 			int      lenWord = kv[0].length();
@@ -73,11 +103,10 @@ public class CleanupPhrases
 			String   c1      = kv[0].substring(0, 1);
 			String   c2      = kv[0].substring(1, 2);
 			if (lenWord == 2 && !isNoun) {
-				if ((freqWords.contains(c1) && freqWords.contains(c2))
-						|| (singleWords.contains(c1) || singleWords.contains(c2))) {
+				if ((freqWords.contains(c1) && freqWords.contains(c2))) {
 					// 高频字组成的词
 					try {
-						fw2.write(String.format("%s\t%s\n", kv[0], kv[1]));
+						fw1.write(String.format("%s\t%s\n", kv[0], kv[1]));
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
@@ -90,14 +119,14 @@ public class CleanupPhrases
 		});
 	}
 
-	private static void cleanupPhrases3(Set<String> codeSet,
+	private static void cleanupPhrases1(List<String> phrases, 
+										Set<String> codeSet,
 	                                    Set<String> phraseCodeSet,
 	                                    Set<String> nounsSet,
 	                                    FileWriter fw1,
 	                                    FileWriter fw2)
 	{
-		List<String> phrases3 = ResourcesFiles.readLinesInResources("phrases-3.txt");
-		phrases3.forEach(line -> {
+		phrases.forEach(line -> {
 			if (line.trim().isEmpty()) {
 				return;
 			}
@@ -117,14 +146,14 @@ public class CleanupPhrases
 		if (isNoun) {
 			phraseCodeSet.add(kv[1]);
 			try {
-				fw2.write(String.format("%s\t%s\n", kv[0], kv[1]));
+				fw1.write(String.format("%s\t%s\n", kv[0], kv[1]));
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		} else if (isDup) {
 			// 与字重
 			try {
-				fw1.write(String.format("%s\t%s\n", kv[0], kv[1]));
+				fw2.write(String.format("%s\t%s\n", kv[0], kv[1]));
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -132,14 +161,14 @@ public class CleanupPhrases
 			if (phraseCodeSet.contains(kv[1])) {
 				// 重码词
 				try {
-					fw1.write(String.format("%s\t%s\n", kv[0], kv[1]));
+					fw2.write(String.format("%s\t%s\n", kv[0], kv[1]));
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			} else {
 				phraseCodeSet.add(kv[1]);
 				try {
-					fw2.write(String.format("%s\t%s\n", kv[0], kv[1]));
+					fw1.write(String.format("%s\t%s\n", kv[0], kv[1]));
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
